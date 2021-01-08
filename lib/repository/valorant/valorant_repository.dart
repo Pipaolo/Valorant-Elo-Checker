804b3ea983d7b44a779b9cdcb4842da2a2e5b56c
Paolo Matthew Tolentino <tolentinopaolomatthew23@gmail.com>
2021-01-08 14:34:55 +0800

    Try to fix some users not getting their data

--- lib/repository/valorant/valorant_repository.dart
+++ lib/repository/valorant/valorant_repository.dart
@@ -1,22 +1,26 @@
 import 'dart:convert';
 
-import "package:http/http.dart" as http;
+import 'package:dio/dio.dart';
 import 'package:valorant_elo_tracker/repository/user/models/user.dart';
 import 'package:valorant_elo_tracker/repository/valorant/models/valorant_match.dart';
 
 class ValorantRepository {
+  final Dio dio = Dio();
+
   Future<List<ValorantMatch>> getCompetitiveDetails(User user) async {
-    final response = await http.get(
+    final response = await dio.get(
       'https://pd.${user.region}.a.pvp.net/mmr/v1/players/${user.id}/competitiveupdates?startIndex=0&endIndex=20',
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${user.accessToken}',
-        'X-Riot-Entitlements-JWT': '${user.entitlementsToken}',
-      },
+      options: Options(
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ${user.accessToken}',
+          'X-Riot-Entitlements-JWT': '${user.entitlementsToken}',
+        },
+      ),
     );
 
     if (response.statusCode == 201 || response.statusCode == 200) {
-      final rawMatches = jsonDecode(response.body)['Matches'];
+      final rawMatches = jsonDecode(response.data)['Matches'];
 
       final matches = (rawMatches as List<dynamic>)
           .map((match) => ValorantMatch.fromMap(match))
